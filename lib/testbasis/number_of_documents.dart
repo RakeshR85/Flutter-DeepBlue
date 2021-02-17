1be0932fa80ae001ad2fdb1914989a134b66227f
RakeshR85 <65712637+RakeshR85@users.noreply.github.com>
2021-02-17 13:40:15 +0000

    fix for count related issue

--- lib/testbasis/number_of_documents.dart
+++ lib/testbasis/number_of_documents.dart
@@ -3,14 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Documents {
   final patientsRef = FirebaseFirestore.instance.collection('registration');
   var count = 0;
-  getNumber() {
-    patientsRef.get().then((QuerySnapshot snapshot) {
+  getNumber() async{
+   await patientsRef.get().then((QuerySnapshot snapshot) {
       snapshot.docs.forEach((DocumentSnapshot documentSnapshot) {
         count = count + 1;
         documentSnapshot.data();
       });
-      print(count);
+      print("In getNumbers Method $count");
     });
-    return count;
+   return count;
   }
 }
